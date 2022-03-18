e367059ec7a4a9fc0fb62d9e2ef16bd521e1bad1
keddib <keddib@student.1337.ma>
2022-03-18 14:10:30 +0100

    templated class to create linked lists

--- linked_list/SinglyLinkedList.hpp
+++ linked_list/SinglyLinkedList.hpp
@@ -11,15 +11,16 @@ class Node {
 	public:
 		Node(const T &x, Node *p = 0) : element(x), next(p) {}
 		~Node() {}
-}
+};
 
 
 template <typename T>
-class SinglyLinkedList
+class List
 {
 	private:
 		/* data */
-		Node *_head;
+		Node *_head, *_tail;
+		size_t _size;
 	public:
 		// types
 		typedef T			type;
@@ -28,25 +29,99 @@ class SinglyLinkedList
 		typedef const T&	const_reference;
 
 
-		SinglyLinkedList() : _head(NULL) {
+		List() : _head(NULL), _size(0) {
+			_head = _tail = 0;
+			_size = 0;
 		}
 
-		SinglyLinkedList(const_reference x) {
+		List(const_reference x) {
 			_head = new Node(x);
+			_tail = _head;
+			_size = 1;
 		}
-		~SinglyLinkedList();
+		~List() {}
 
-		void addNode(const_reference x) {
 
-			Node *p = new Node(x);
-			if (_head) {
-				Node *tmp = _head;
-				while (tmp)
-					tmp = tmp->next;
-				tmp->next = p;
+		bool isEmpty() { return _head == 0; }
+
+		size_t length() const { return _size; }
+
+		void addToHead(const_reference x) {
+			_head = new Node(x, _head);
+			if (_tail == 0)
+				_tail = _head;
+			_size++;
+		}
+
+		void addToTail(const_reference x) {
+			if (_tail) { // list not empty
+				_tail->next = new Node(x);
+				_tail = _tail->next;
+			} else _head = _tail = new Node(x);
+			_size++;
+		}
+
+		void deleleNode(const_reference x) {
+			Node *tmp, *prev;
+			if (_head == _tail && _head && _head->element == x) { // if one node
+				delete _head
+				_head = _tail = NULL;
 			}
-			_head = p;
+			else if (_head && _head->element == x) {
+				tmp = _head;
+				_head = _head->next;
+				delete tmp;
+			}
+			for ( = _head; tmp != NULL; tmp = tmp->next) {
+				if (tmp->element == x){
+					Node *t = tmp;
+					tmp = tmp->next;
+				}
+			}
 		}
+
+		int deleleFromHead() {
+			if (_head) {
+				type x = _head->element;
+				Node *tmp = _head;
+				if (_head == _tail)
+					_head = _tail = 0;
+				else
+					_head = _head->next;
+				delete tmp;
+				_size--;
+				return x;
+			}
+			return 0;
+		}
+
+		void deleleFromTail() {
+			if (_tail) {
+				type x = _tail->element;
+				if (_tail == _head) {
+					delete _tail;
+					_tail = _head = NULL;
+				} else {
+					Node *tmp;
+					for (tmp = _head; tmp->next != _tail; tmp = tmp->next); // fine last element befor tail
+					delete _tail;
+					_tail = tmp;
+					_tail->next = NULL;
+				}
+				_size--;
+				return x;
+			}
+		}
+
+		bool isInList(const_reference x) {
+			Node *tmp;
+			for (tmp = _head; tmp != NULL; tmp = tmp->next)
+				if (tmp->element == x)
+					return 1;
+			return 0;
+		}
+
+
 };
 
 
